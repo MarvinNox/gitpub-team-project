fa750fa6cfe201029ddf411c315ec144dd754a13
Marvin Nox <sinedtex@gmail.com>
2025-04-25 16:07:29 +0300

    add theme switch js func

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,4 +1,32 @@
 export const refs = {
     THEME_KEY: 'theme',
-    switcher: document.querySelector('#theme-switcher')
+    themeToggler: document.querySelector('#theme-switcher'),
 };
+
+export function getTheme() {
+    return JSON.parse(localStorage.getItem(refs.THEME_KEY)) || false;
+}
+
+export function setTheme(check) {
+    localStorage.setItem(refs.THEME_KEY, JSON.stringify(check));
+}
+
+export function detectTheme() {
+    const savedTheme = getTheme();
+    if (savedTheme) {
+        refs.themeToggler.checked = true;
+        document.querySelector('body').classList.add('dark');
+    }
+}
+export function toggleSwitcher(evt) {
+    if (evt.target.checked) {
+        document.body.classList.add('dark');
+        setTheme(true);
+    } else {
+        setTheme(false);
+        document.body.classList.remove('dark');
+    }
+}
+detectTheme();
+console.log(refs.themeToggler);
+refs.themeToggler.addEventListener('change', toggleSwitcher);
