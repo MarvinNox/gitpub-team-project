3d57e5f96d108a129961fb573156bba209def571
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-04-25 22:53:28 +0200

    ispravil

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {loadNextProjects} from './js/my-projects'
+// Загрузка первых 3-х проектов при старте страницы (без скролла) в секции my-project
+loadNextProjects(false);
