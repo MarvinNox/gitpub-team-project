4a27d34a32ef34156ec373e7629f48acda9b9a92
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-04-26 12:55:57 +0200

    ispravil kartinki dobavil norm kaczestvo i hover na visit i visit teper bez a prosto button

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -3,63 +3,63 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const allProjects = [
     {
-        src1x: 'my-projects/rectangle-11-1x.webp',  
-        src2x: 'my-projects/rectangle-11-2x.webp',
-        src: '/my-projects/rectangle-11-1x.webp',
+        src1x: '/my-projects/rectangle-11-1x.jpg',  
+        src2x: '/my-projects/rectangle-11-2x.jpg',
+        src: '/my-projects/rectangle-11-1x.jpg',
         alt: "Wallet webservice "
     },
     {
-        src1x: 'my-projects/rectangle-9-1x.webp',  
-        src2x: 'my-projects/rectangle-9-2x.webp',
-        src: 'my-projects/rectangle-9-1x.webp',
+        src1x: '/my-projects/rectangle-9-1x.jpg',  
+        src2x: '/my-projects/rectangle-9-2x.jpg',
+        src: '/my-projects/rectangle-9-1x.jpg',
         alt: "Green harvest webservice"
     },
     {
-        src1x: 'my-projects/rectangle-12-1x.webp',  
-        src2x: 'my-projects/rectangle-12-2x.webp',
-        src: 'my-projects/rectangle-12-1x.webp',
+        src1x: '/my-projects/rectangle-12-1x.jpg',  
+        src2x: '/my-projects/rectangle-12-2x.jpg',
+        src: '/my-projects/rectangle-12-1x.jpg',
         alt: "English Exellence website"
     },
     {
-        src1x: '/my-projects/rectangle-10-1x.webp',  
-        src2x: '/my-projects/rectangle-10-2x.webp',
-        src: '/my-projects/rectangle-10-1x.webp',
+        src1x: '/my-projects/rectangle-10-1x.jpg',  
+        src2x: '/my-projects/rectangle-10-2x.jpg',
+        src: '/my-projects/rectangle-10-1x.jpg',
         alt: "power pulse webservice"
     },
     {
-        src1x: '/my-projects/rectangle-7-1x.webp',
-        src2x: '/my-projects/rectangle-7-2x.webp',
-        src: '/my-projects/rectangle-7-1x.webp',
+        src1x: '/my-projects/rectangle-7-1x.jpg',
+        src2x: '/my-projects/rectangle-7-2x.jpg',
+        src: '/my-projects/rectangle-7-1x.jpg',
         alt: "mimino website"
     },
     {
-        src1x: '/my-projects/rectangle-8-1x.webp',
-        src2x: '/my-projects/rectangle-8-2x.webp',
-        src: '/my-projects/rectangle-8-1x.webp',
+        src1x: '/my-projects/rectangle-8-1x.jpg',
+        src2x: '/my-projects/rectangle-8-2x.jpg',
+        src: '/my-projects/rectangle-8-1x.jpg',
         alt: "vyshyvanka vibes Landing Page"
     },
     {
-        src1x: '/my-projects/rectangle-6-1x.webp',
-        src2x: '/my-projects/rectangle-6-2x.webp',
-        src: '/my-projects/rectangle-6-1x.webp',
+        src1x: '/my-projects/rectangle-6-1x.jpg',
+        src2x: '/my-projects/rectangle-6-2x.jpg',
+        src: '/my-projects/rectangle-6-1x.jpg',
         alt: "chego jewelry website"
     },
     {
-        src1x: '/my-projects/rectangle-4-1x.webp',
-        src2x: '/my-projects/rectangle-4-2x.webp',
-        src: '/my-projects/rectangle-4-1x.webp',
+        src1x: '/my-projects/rectangle-4-1x.jpg',
+        src2x: '/my-projects/rectangle-4-2x.jpg',
+        src: '/my-projects/rectangle-4-1x.jpg',
         alt: "energy flow webservice"
     },
     {
-        src1x: '/my-projects/rectangle-5-1x.webp',
-        src2x: '/my-projects/rectangle-5-2x.webp',
-        src: '/my-projects/rectangle-5-1x.webp',
+        src1x: '/my-projects/rectangle-5-1x.jpg',
+        src2x: '/my-projects/rectangle-5-2x.jpg',
+        src: '/my-projects/rectangle-5-1x.jpg',
         alt: "fruitbox online store"
     },
     {
-        src1x: '/my-projects/first-screen-1-1x.webp',
-        src2x: '/my-projects/first-screen-1-2x.webp',
-        src: '/my-projects/first-screen-1-1x.webp',
+        src1x: '/my-projects/first-screen-1-1x.jpg',
+        src2x: '/my-projects/first-screen-1-2x.jpg',
+        src: '/my-projects/first-screen-1-1x.jpg',
         alt: "starlight studio landing page"
     }
 ];
@@ -80,22 +80,20 @@ function createGallery(projects) {
     .map(({ src1x, src2x, src, alt, h3 }) => {
       return `
       <li>
-        <a href="${src}" class="gallery-link">
-          <img class="image-my-project" srcset="${src1x} 1x, ${src2x} 2x" src="${src}" alt="${alt}" />
-        </a>
-        <div class="div-my-project">
-          <p class="text-my-project">React, JavaScript, Node JS, Git</p>
-          <h3 class="names-my-project">${alt}</h3>
-          <a href="https://github.com/Marakasss/gitpub-team-project" target="_blank">
-            <button class="button-my-project">
-              <span class="visit-my-project">VISIT</span>
-              <svg class="svg-my-project" height="14.5" width="14.5">
-                <use class="svg-visit-my-project" href="/img/sprite.svg#icon-visit-arrow"></use>
-              </svg>
-            </button>
-          </a>
-        </div>
-      </li>
+  <a href="${src}" class="gallery-link">
+    <img class="image-my-project" srcset="${src1x} 1x, ${src2x} 2x" src="${src}" alt="${alt}" />
+  </a>
+  <div class="div-my-project">
+    <p class="text-my-project">React, JavaScript, Node JS, Git</p>
+    <h3 class="names-my-project">${alt}</h3>
+    <button class="button-my-project" onclick="window.open('https://github.com/Marakasss/gitpub-team-project', '_blank')">
+      <span class="visit-my-project">VISIT</span>
+      <svg class="svg-my-project" height="14.5" width="14.5">
+        <use class="svg-visit-my-project" href="/img/sprite.svg#icon-visit-arrow"></use>
+      </svg>
+    </button>
+  </div>
+</li>
     `;
     })
     .join('');
@@ -140,5 +138,3 @@ function createGallery(projects) {
 
 // Обработчик кнопки "LOAD MORE" — включает скролл
 loadMoreBtn.addEventListener('click',loadNextProjects);
-
-
