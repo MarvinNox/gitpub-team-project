8c49cfc27639debdc7377811de9b845900227d89
Marvin Nox <sinedtex@gmail.com>
2025-04-26 11:30:55 +0300

    add scroll btn html+js+css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { detectTheme, refs, toggleSwitcher } from "./js/theme-switcher";
+import { refsScroll, scrollUpBtnShow, scrollUp } from "./js/scroll-up"
 
 detectTheme();
 refs.themeToggler.addEventListener('change', toggleSwitcher);
+
+window.addEventListener('scroll', scrollUpBtnShow);
+refsScroll.scrollBtn.addEventListener('click', scrollUp);
